58b70c55393fb07c0bd923b0c9888c2598da28a7
DeltaMokey <engincanozkan@outlook.com>
2023-12-16 13:23:32 +0300

    as no tracking parameter added to repository

--- DotnetAngularMiniEcommerce_API/Core/DotnetAngularMiniEcommerce_API.Application/Repositories/IReadRepository.cs
+++ DotnetAngularMiniEcommerce_API/Core/DotnetAngularMiniEcommerce_API.Application/Repositories/IReadRepository.cs
@@ -5,9 +5,9 @@ namespace DotnetAngularMiniEcommerce_API.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
-        Task<T> GetFirstAsync(Expression<Func<T, bool>> method);
-        Task<T> GetByIdAsync(string id);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetFirstAsync(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
     }
 }
--- DotnetAngularMiniEcommerce_API/Presentation/DotnetAngularMiniEcommerce_API.API/Controllers/ProductsController.cs
+++ DotnetAngularMiniEcommerce_API/Presentation/DotnetAngularMiniEcommerce_API.API/Controllers/ProductsController.cs
@@ -23,12 +23,15 @@ namespace DotnetAngularMiniEcommerce_API.API.Controllers
         [HttpGet]
         public async Task Get() 
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() { ID = Guid.NewGuid(), Name = "Product_1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10 },
-                new() { ID = Guid.NewGuid(), Name = "Product_2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20 },
-                new() { ID = Guid.NewGuid(), Name = "Product_3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 130 }
-            });
+            ///await _productWriteRepository.AddRangeAsync(new()
+            ///{
+            ///    new() { ID = Guid.NewGuid(), Name = "Product_1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10 },
+            ///    new() { ID = Guid.NewGuid(), Name = "Product_2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20 },
+            ///    new() { ID = Guid.NewGuid(), Name = "Product_3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 130 }
+            ///});
+            ///await _productWriteRepository.SaveAsync();
+            Product p = await _productReadRepository.GetByIdAsync("879af56d-0fbf-41e4-a17c-86cf90983acd", false);
+            p.Name = "ABC";
             await _productWriteRepository.SaveAsync();
         }
 
