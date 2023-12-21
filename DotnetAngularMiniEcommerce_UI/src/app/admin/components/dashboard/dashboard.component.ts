c1b64741ef33ceab1639fafda5f7e3c57b3c04d3
DeltaMokey <engincanozkan@outlook.com>
2023-12-21 22:22:28 +0300

    NgxSpinnerImplementation

--- DotnetAngularMiniEcommerce_UI/src/app/admin/components/dashboard/dashboard.component.ts
+++ DotnetAngularMiniEcommerce_UI/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AleritfyService, MessageType, Position } from 'src/app/services/admin/aleritfy.service';
 
 @Component({
@@ -6,9 +8,18 @@ import { AleritfyService, MessageType, Position } from 'src/app/services/admin/a
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify: AleritfyService) { }
+  constructor(
+    spinner: NgxSpinnerService,
+    private alertify: AleritfyService
+    ) { 
+    super(spinner);
+  }
+
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallSpinClockwiseFadeRotating);
+  }
 
   m() {
     this.alertify.message("merhaba", { 
--- DotnetAngularMiniEcommerce_UI/src/app/app.component.ts
+++ DotnetAngularMiniEcommerce_UI/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
-declare var $: any
 
 @Component({
   selector: 'app-root',
@@ -9,20 +7,6 @@ declare var $: any
 })
 export class AppComponent {
   title = 'DotnetAngularMiniEcommerce_UI';
-
-  constructor(private toastrService: CustomToastrService) {
-    toastrService.message("merhaba", "engin", { 
-      messageType: ToastrMessageType.Success, 
-      position: ToastrPosition.TopCenter 
-    });
-    toastrService.message("merhaba", "engin", { 
-      messageType: ToastrMessageType.Error, 
-      position: ToastrPosition.TopCenter 
-    });
-    toastrService.message("merhaba", "engin", { 
-      messageType: ToastrMessageType.Warning, 
-      position: ToastrPosition.TopCenter 
-    });
-    toastrService.message("merhaba", "engin");
-  }
+  
+  constructor() { }
 }
